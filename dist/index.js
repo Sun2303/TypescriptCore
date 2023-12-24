08349e04788e58d91b1e7caa04e6363676185c65
Sun Automation <sun.libra95@gmail.com>
2023-12-24 12:25:50 +0700

    Typescript core

---
+++ dist/index.js
@@ -0,0 +1,10 @@
+"use strict";
+let age = 20;
+if (age < 50)
+    age += 10;
+console.log(age);
+let user = [1, 'a', true];
+;
+let mySize = 3;
+console.log(mySize);
+//# sourceMappingURL=index.js.map
---
+++ src/index.ts
@@ -0,0 +1,11 @@
+let age: number = 20;
+if (age < 50)
+    age += 10;
+console.log(age);
+
+let user: [number, string, boolean] = [1, 'a', true];
+
+//PascalCase
+const enum Size { Small = 1, Medium, Large };
+let mySize: Size = Size.Large;
+console.log(mySize);
