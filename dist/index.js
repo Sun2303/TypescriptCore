a69804e5ee69ff8f7affae66d7b3775319510a61
Sun Automation <sun.libra95@gmail.com>
2023-12-25 23:22:58 +0700

    basic TS core

--- dist/index.js
+++ dist/index.js
@@ -7,4 +7,34 @@ let user = [1, 'a', true];
 ;
 let mySize = 3;
 console.log(mySize);
+function calculateTax(income, taxYear = 2024) {
+    if (taxYear < 2023) {
+        return income * 1.5;
+    }
+    return income * 2;
+}
+console.log(calculateTax(100, 2021));
+let employee = {
+    id: 1,
+    name: 'Sun',
+    retire: (date) => {
+        console.log(date);
+    },
+};
+let sun = {
+    id: 2,
+    name: 'Sun CD',
+    retire: (date) => {
+        console.log(date);
+    }
+};
+function kgToLbs(weight) {
+    if (typeof weight === 'number') {
+        return weight * 2.0;
+        ;
+    }
+    else {
+        return parseInt(weight) * 1.5;
+    }
+}
 //# sourceMappingURL=index.js.map
--- src/index.ts
+++ src/index.ts
@@ -9,3 +9,45 @@ let user: [number, string, boolean] = [1, 'a', true];
 const enum Size { Small = 1, Medium, Large };
 let mySize: Size = Size.Large;
 console.log(mySize);
+
+function calculateTax(income: number, taxYear = 2024): number {
+    if (taxYear < 2023) {
+        return income * 1.5;
+    }
+    return income * 2;
+}
+console.log(calculateTax(100, 2021));
+
+//Type Aliases
+type Employee = {
+    readonly id: number,
+    name: string,
+    retire: (date: Date) => void
+}
+
+//Objects
+let employee: Employee = {
+    id: 1,
+    name: 'Sun',
+    retire: (date: Date) => {
+        console.log(date);
+    },
+}
+
+let sun: Employee = {
+    id: 2,
+    name: 'Sun CD',
+    retire: (date: Date) => {
+        console.log(date);
+    }
+}
+
+//Union Type
+function kgToLbs(weight: number | string): number {
+    if (typeof weight === 'number') {
+        return weight * 2.0;;
+    } else {
+        return parseInt(weight) * 1.5;
+    } 
+}
+
